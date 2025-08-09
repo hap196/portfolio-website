f68f3f597cb18df689249ef098364b3f09a0e5a9
Hailey Pan <haileyrpan@gmail.com>
2025-08-08 19:37:39 -0500

    create hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+// src/components/Hero.tsx
+const Hero = () => {
+  return (
+    <div className="text-center flex-1 flex items-center relative">
+      {/* Background Names - Light White */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
+        {/* Top Background Name */}
+        <h1 className="font-playfair text-8xl lg:text-9xl font-normal text-white/20 tracking-wide transform -translate-y-16">
+          Hailey Pan
+        </h1>
+
+        {/* Bottom Background Name */}
+        <h1 className="font-playfair text-8xl lg:text-9xl font-normal text-white/20 tracking-wide transform translate-y-16">
+          Hailey Pan
+        </h1>
+      </div>
+
+      {/* Main Name */}
+      <h1 className="font-playfair text-8xl lg:text-9xl font-normal text-text-dark tracking-wide relative z-10">
+        Hailey Pan
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
+
---
+++ src/components/Roles.tsx
@@ -0,0 +1,19 @@
+const Roles = () => {
+  return (
+    <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2">
+      <div className="flex space-x-12 text-center">
+        <span className="font-playfair text-3xl text-text-dark tracking-wide">
+          Developer
+        </span>
+        <span className="font-playfair text-3xl text-text-dark tracking-wide">
+          Student
+        </span>
+        <span className="font-playfair text-3xl text-text-dark tracking-wide">
+          Founder
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Roles;
