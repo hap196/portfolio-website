dc63bc543eb47ff6566e5c42c81bd0b686fe3623
Hailey Pan <haileyrpan@gmail.com>
2025-10-25 16:20:56 -0700

    writing

---
+++ app/writing/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer";
+
+export default function Writing() {
+  return (
+    <main className="min-h-screen bg-[var(--color-background)] pt-28 flex flex-col">
+      <div className="max-w-3xl px-10 container mx-auto px-8 py-12 flex-grow">
+        <h1 className="text-4xl font-light text-[var(--color-text-primary)] font-serif mb-12">
+          Writing
+        </h1>
+
+        <p className="text-lg text-[var(--color-text-secondary)] font-serif">
+          coming soon...
+        </p>
+      </div>
+      <Footer />
+    </main>
+  );
+}
